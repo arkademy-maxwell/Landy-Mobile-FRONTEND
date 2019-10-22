b841a5c57085c7eb7d96f5ea320f5d6446ac03dc
Budi Prih Hastomo <budi.prihhastomo27@gmail.com>
2019-10-22 22:12:48 +0700

    Fix Issue General Dependency

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,42 +1,57 @@
-import React, { Component } from 'react';
-import { Container, Content, Form, Item, Input, Label, View, Tab, Tabs, Button, Text } from 'native-base';
+import React, {Component} from 'react';
+import {
+  Container,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  View,
+  Tab,
+  Tabs,
+  Button,
+  Text,
+} from 'native-base';
 
 // component
-import HeaderBanner from './Component/HeaderBanner'
+import HeaderBanner from './Component/HeaderBanner';
 import ScreenRoom from './Screens/Rooms';
 import ScreenPlane from './Screens/Plane';
 
 const Rooms = props => {
-    return (
-        <Content>
-            <HeaderBanner />
-            <View>
-                <Tabs
-                    tabBarUnderlineStyle={{ borderBottomWidth: 2, borderColor: '#1bb6f1' }} tabContainerStyle={{ elevation: 0 }}>
-                    <Tab heading="Kamar"
-                        tabStyle={{ backgroundColor: '#fff' }}
-                        textStyle={{ color: '#000' }}
-                        activeTabStyle={{ backgroundColor: '#fff' }}
-                        activeTextStyle={{
-                            color: '#d0d4d7',
-                            fontWeight: 'normal',
-                            borderColor: 'red'
-                        }}>
-                        <ScreenRoom navigate={props.navigation.navigate} />
-                    </Tab>
-                    <Tab heading="Tiket Pesawat"
-                        tabStyle={{ backgroundColor: '#fff' }}
-                        textStyle={{ color: '#000' }}
-                        activeTabStyle={{ backgroundColor: '#fff' }}
-                        activeTextStyle={{
-                            color: '#d0d4d7',
-                            fontWeight: 'normal'
-                        }}>
-                        <ScreenPlane />
-                    </Tab>
-                </Tabs>
-            </View>
-        </Content>
-    );
-}
-export default Rooms
+  return (
+    <Content>
+      <HeaderBanner />
+      <View>
+        <Tabs
+          tabBarUnderlineStyle={{borderBottomWidth: 2, borderColor: '#1bb6f1'}}
+          tabContainerStyle={{elevation: 0}}>
+          <Tab
+            heading="Kamar"
+            tabStyle={{backgroundColor: '#fff'}}
+            textStyle={{color: '#000'}}
+            activeTabStyle={{backgroundColor: '#fff'}}
+            activeTextStyle={{
+              color: '#d0d4d7',
+              fontWeight: 'normal',
+              borderColor: 'red',
+            }}>
+            <ScreenRoom navigate={props.navigation.navigate} />
+          </Tab>
+          <Tab
+            heading="Tiket Pesawat"
+            tabStyle={{backgroundColor: '#fff'}}
+            textStyle={{color: '#000'}}
+            activeTabStyle={{backgroundColor: '#fff'}}
+            activeTextStyle={{
+              color: '#d0d4d7',
+              fontWeight: 'normal',
+            }}>
+            <ScreenPlane />
+          </Tab>
+        </Tabs>
+      </View>
+    </Content>
+  );
+};
+export default Rooms;
