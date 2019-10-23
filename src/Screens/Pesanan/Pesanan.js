318added0c827131e368a0060205a5ef03b469f9
Budi Prih Hastomo <budi.prihhastomo27@gmail.com>
2019-10-23 10:53:23 +0700

    Build Pesanan Layout [Fixed]

--- src/Screens/Pesanan/Pesanan.js
+++ src/Screens/Pesanan/Pesanan.js
@@ -1,13 +1,65 @@
 import React from 'react';
-import { Text, Container } from 'native-base'
-// import { styles } from './Home.style';
+import {ScrollView, TouchableOpacity, ImageBackground} from 'react-native';
+import {Text, Container, View, Icon} from 'native-base';
+import {styles} from './Pesanan.style';
 
 const Pesanan = props => {
-    return (
-        <Container>
-            <Text>Pesanan</Text>
-        </Container>
-    )
-}
+  const _renderEmptyCart = props => (
+    <View style={styles.mainContent}>
+      <ImageBackground
+        source={require('../../../public/assets/Image/ic_empty_cart.png')}
+        style={{width: 170, height: 100, alignSelf: 'center'}}
+      />
+      <View style={styles.textSection}>
+        <Text style={styles.mainText}>Belum ada pembelian</Text>
+        <Text style={styles.secondText}>
+          Pembelian yang sedang berlangsung akan ditampilkan disini
+        </Text>
+        <Text style={styles.linkText}>LIHAT BUKTI PEMBELIAN</Text>
+      </View>
+    </View>
+  );
 
-export default Pesanan
+  return (
+    <View style={styles.content}>
+      <View style={styles.header}>
+        <View style={styles.nav}>
+          <Text style={styles.title}>Pesanan Saya</Text>
+        </View>
+        <View style={styles.scrollViewArea}>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <View style={styles.contentHeader}>
+              <View style={styles.mainCard}>
+                <View style={styles.circleTop} />
+                <View style={styles.circleBottom} />
+                <View style={styles.contentMainCard}>
+                  <Text style={styles.textCard}>
+                    Belum Ada Tiket dan Voucher Aktif
+                  </Text>
+                  <Text style={styles.textCard}>
+                    Tiket atau voucher yang telah terbit akan ditampilkan
+                    disini.
+                  </Text>
+                </View>
+              </View>
+              <TouchableOpacity activeOpacity={0.7}>
+                <View style={styles.listCard}>
+                  <View style={styles.iconSection}>
+                    <Icon
+                      name="ios-list-box"
+                      style={{fontSize: 20, color: '#fff'}}
+                    />
+                  </View>
+                  <Text style={styles.textIconSection}>Lihat Semua</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+        </View>
+      </View>
+      {_renderEmptyCart()}
+    </View>
+  );
+};
+
+export default Pesanan;
---
+++ src/Screens/Pesanan/Pesanan.style.js
@@ -0,0 +1,116 @@
+import {Dimensions, StyleSheet} from 'react-native';
+
+const HEIGHT_DEVICE = Dimensions.get('window').height;
+const WIDTH_DEVICE = Dimensions.get('window').width;
+export const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#017fef',
+    width: '100%',
+    height: 220,
+  },
+  nav: {
+    backgroundColor: '#0191ef',
+    height: 60,
+    justifyContent: 'center',
+    paddingLeft: 20,
+  },
+  title: {
+    color: '#fff',
+  },
+  scrollViewArea: {
+    flex: 1,
+    paddingVertical: 20,
+  },
+  contentHeader: {
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+  },
+  mainCard: {
+    width: WIDTH_DEVICE - 130,
+    height: 120,
+    justifyContent: 'center',
+    backgroundColor: '#00aeef',
+    borderRadius: 5,
+    marginRight: 10,
+    elevation: 2,
+  },
+  listCard: {
+    width: 140,
+    height: 120,
+    backgroundColor: '#00aeef',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  circleTop: {
+    backgroundColor: '#017fef',
+    width: 20,
+    height: 10,
+    position: 'absolute',
+    right: 70,
+    top: 0,
+    borderBottomStartRadius: 50,
+    borderBottomEndRadius: 50,
+  },
+  circleBottom: {
+    backgroundColor: '#017fef',
+    width: 20,
+    height: 10,
+    position: 'absolute',
+    right: 70,
+    bottom: 0,
+    borderTopStartRadius: 50,
+    borderTopEndRadius: 50,
+  },
+  contentMainCard: {
+    paddingHorizontal: 20,
+  },
+  textCard: {
+    color: '#fff',
+    fontSize: 12,
+  },
+  iconSection: {
+    width: 40,
+    height: 40,
+    backgroundColor: '#18b5f1',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+  },
+  textIconSection: {
+    color: '#fff',
+    fontSize: 12,
+    marginTop: 5,
+    textDecorationLine: 'underline',
+  },
+  mainContent: {
+    flex: 1,
+    paddingVertical: 80,
+    backgroundColor: '#ecf0f1',
+  },
+  mainText: {
+    color: '#191919',
+    textAlign: 'center',
+    fontSize: 18,
+    marginBottom: 5,
+    fontWeight: 'bold',
+  },
+  secondText: {
+    color: '#191919',
+    textAlign: 'center',
+    fontSize: 12,
+    marginBottom: 15,
+  },
+  linkText: {
+    color: '#14b3ef',
+    textAlign: 'center',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  textSection: {
+    marginTop: 10,
+  },
+});
